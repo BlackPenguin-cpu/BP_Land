e60f931aa834b734bac88d228fe89af4ce86d243
Black_Penguin <dkstmdwnswns@gmail.com>
2024-10-29 21:00:40 +0900

    텍스트 애니메이터 편집, 폰트 수정

--- 241021_bpLand/Assets/Script/UI/NpcTextBox.cs
+++ 241021_bpLand/Assets/Script/UI/NpcTextBox.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Febucci.UI;
 
 public class NpcTextBox : MonoBehaviour
 {
     public TextMeshProUGUI npcTextBox;
 
+    private void Update()
+    {
+    }
 }
