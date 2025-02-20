e7fd4ad56c63f5f91c8f76360d4114b4614e914c
Black_Penguin <dkstmdwnswns@gmail.com>
2025-02-20 15:41:21 +0900

    await 실험

--- 241021_bpLand/Assets/Script/InteractiveObj/MainNPC.cs
+++ 241021_bpLand/Assets/Script/InteractiveObj/MainNPC.cs
@@ -2,27 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainNPC : InteractiveObj
 {
     public List<NpcTextBox.EffectTextInfo> npcTextList;
+    public List<NpcTextBox.EffectTextInfo> npcTextList2;
 
     protected override void OnInteractionAction()
     {
         NpcStartTexting();
     }
 
-    private async UniTaskVoid NpcStartTexting()
+    private async UniTask NpcStartTexting()
     {
         await NpcTextBox.Instance.StartText(npcTextList);
         NpcTextBox.Instance.ResetText();
 
         var strs = new List<string>() { "감사합니다", "나가주세요", "싫어요" };
-        var actions = new List<Action>() { null, null, null };
+        var action = new List<Func<UniTask>>();
         var index = await ChooseUI.instance.ChooseSlotInfoAddAndStart(strs);
-        actions[index].Invoke();
 
+        action.Add(NpcTextBox.Instance.StartText(npcTextList2));
+
+        await action[index]();
+        
         OnInteractionOver();
     }
-}
